04b2ea0c1bfbf01dc8d295fc11605a9a76940b21
Kamila <kamkopiczko@gmail.com>
2022-06-03 16:55:04 +0200

    created models for gem and gemproperties table

---
+++ models/gem_models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import table
+from sqlmodel import SQLModel, Field
+from enum import Enum
+from typing import Optional
+
+class GemClarity(Enum):
+    SI = 'SI'
+    VS = 'VS'
+    VVS = 'VVS'
+    FL = 'FL'
+
+class GemColor(Enum):
+    D = 'D'
+    E = 'E'
+    G = 'G'
+    F = 'F'
+    H = 'H'
+    I = 'I'
+
+class GemType(Enum):
+    DIAMOND = 'DIAMOND'
+    EMERALD = 'EMERALD'
+    RUBY = 'RUBY'
+
+class Gem(SQLModel, table=True):
+    id: Field(primary_key=True)
+    type: Optional[GemType] = GemType.DIAMOND
+    properties_id: Optional[int] = Field(default=None, foreign_key='gemproperties.id')
+    price: float = 1
+    is_available: bool = True
+    
+
+class GemProperties(SQLModel, table=True):
+    id: Field(primary_key=True)
+    size: float = 1
+    color: Optional[GemColor] = None
+    clarity: Optional[GemClarity] = None
