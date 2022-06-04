6bd5791754576be1ad0b9b14f4f04fb03348765f
Kamila <kamkopiczko@gmail.com>
2022-06-04 08:22:28 +0200

    created select_gems function that retrieves gems from a db

---
+++ gem_repository.py
@@ -0,0 +1,12 @@
+from main import engine
+from models.gem_models import Gem, GemProperties
+from sqlmodel import Session, select
+
+def select_gems():
+    with Session(bind=engine) as session:
+        statement = select(Gem)
+        # statement = statement.where(Gem.id == 2)
+        result = session.exec(statement)
+        print(result.all())
+
+select_gems()
