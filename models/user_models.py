204588079f7332c8aad0f80e42a58250d0d55f57
Kamila <kamkopiczko@gmail.com>
2022-06-06 16:21:30 +0200

    used validator to check if passwords are same

--- models/user_models.py
+++ models/user_models.py
@@ -1,13 +1,14 @@
 from sqlalchemy import table
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
+from pydantic import validator, EmailStr
 import datetime
 
 class User(SQLModel, table=True):
     id: Optional[str] = Field(primary_key=True)
     username: str
     password: str
-    email: str
+    email: EmailStr
     created_at: datetime.datetime = datetime.datetime.now()
     is_seller: bool = False
 
@@ -15,4 +16,10 @@ class UserCreate(SQLModel):
     username: str
     password: str = Field(max_length=256, min_length=6)
     password2: str
-    email: str
+    email: EmailStr
+
+    @validator('password2')
+    def password_match(cls, v, values, **kwargs):
+        if 'password' in values and v != values['password']:
+            raise ValueError('passwords don\'t match')
+        return v
