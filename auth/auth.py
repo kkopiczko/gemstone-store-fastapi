949038a0cbe01838667f69376183657a4a453506
Kamila <kamkopiczko@gmail.com>
2022-06-08 12:19:08 +0200

    creted utility functions for authorization with jwt

---
+++ auth/auth.py
@@ -0,0 +1,36 @@
+from fastapi import HTTPException, status, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from passlib.context import CryptContext
+import datetime
+import jwt
+
+class AuthHandler:
+    security = HTTPBearer()
+    pwd_context = CryptContext(schemas=['bcrypt'])
+    secret = 'supersecret'
+
+    def get_password_hast(self, plain_password):
+        return self.pwd_context.hash(plain_password)
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def encode_token(self, user_id):
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(payload, self.secret, algorithm='HS256')
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=['HS256'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Expired signature')
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+        return self.decode_token(auth.credentials)
--- routers/users.py
+++ routers/users.py
@@ -1,3 +1,5 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix='/users', tags=['Users'])
+
+# router.get('/register')
